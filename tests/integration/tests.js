16d50209fe5304cf1ebb063e9b2347462ee50bd3
Jim Bevington <jim.bevington@hotmail.com>
2018-04-20 11:32:18 +0100

    Added chaining operations integration test

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -61,8 +61,23 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('35');
   })
 
-  // - Do each of the arithmetical operations work to update the display with the result of the operation?
   // - Can we chain multiple operations together?
+  it('should be able to chain operations together', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4');
+
+  })
   // - Does it work as expected for a range of numbers? (positive, negative, decimals, very large numbers)
 
 });
