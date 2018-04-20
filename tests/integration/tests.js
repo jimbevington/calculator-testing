2698306ef639324ff63395072a145bc1d8acc7fb
Jim Bevington <jim.bevington@hotmail.com>
2018-04-20 11:38:29 +0100

    added working as expected for positive and negative numbers tests

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -76,7 +76,29 @@ describe('calculator functionality', function() {
     element(by.css('#number4')).click();
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('4');
+  })
 
+  it('works as expected with positive numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('110');
+  })
+
+  it('works as expected with negative numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-7');
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-14');
   })
   // - Does it work as expected for a range of numbers? (positive, negative, decimals, very large numbers)
 
